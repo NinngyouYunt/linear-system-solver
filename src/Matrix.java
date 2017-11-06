88436cfc4ecdafa4b7fb7c4a5985c31569efe078
Peixin Liang <peterliangap@outlook.com>
2017-11-06 15:59:57 -0500

    Creating Matrix class

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,8 +1,36 @@
+import java.util.Arrays;
 
 public class Matrix {
-
+	
+	// Order of matrix
+	private int m, n;
+	
+	private double[][] elements;
+	
+	
+	/**
+	 * 
+	 * @param rowNum
+	 * @param colNum
+	 */
+	public Matrix(int rowNum, int colNum){
+		m  = rowNum;
+		n = colNum;
+		elements = new double[m][n];
+	}
+	
+	// Overwrite toString()
+	public String toString(){
+		String res="";
+		for(int i=0;i<m;i++){
+			res = res + Arrays.toString(elements[i]) + "\n";
+		}
+		return res;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Matrix A = new Matrix(2, 2);
+		System.out.println(A);
 	}
 
 }
